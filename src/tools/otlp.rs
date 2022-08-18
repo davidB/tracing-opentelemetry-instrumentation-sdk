616d0767470a050e550c98f20b6ad11502e454d9
David Bernard <david.bernard.31@gmail.com>
2022-08-19 00:26:30 +0200

    :sparkles: use `OTEL_EXPORTER_OTLP_...` environment variable to init tracer for otlp

--- src/tools/otlp.rs
+++ src/tools/otlp.rs
@@ -2,11 +2,13 @@ use opentelemetry::sdk::Resource;
 use opentelemetry::{
     global, sdk::propagation::TraceContextPropagator, sdk::trace as sdktrace, trace::TraceError,
 };
+use opentelemetry_otlp::SpanExporterBuilder;
 
 pub fn identity(v: opentelemetry_otlp::OtlpTracePipeline) -> opentelemetry_otlp::OtlpTracePipeline {
     v
 }
 
+// see https://opentelemetry.io/docs/reference/specification/protocol/exporter/
 pub fn init_tracer<F>(resource: Resource, transform: F) -> Result<sdktrace::Tracer, TraceError>
 where
     F: FnOnce(opentelemetry_otlp::OtlpTracePipeline) -> opentelemetry_otlp::OtlpTracePipeline,
@@ -14,15 +16,24 @@ where
     use opentelemetry_otlp::WithExportConfig;
 
     global::set_text_map_propagator(TraceContextPropagator::new());
-    // TODO try to autodetect if http or grpc should be used (eg based on env variable, port ???)
-    //endpoint (default = 0.0.0.0:4317 for grpc protocol, 0.0.0.0:4318 http protocol):
-    //.http().with_endpoint(collector_url),
-    let endpoint_grpc = std::env::var("OTEL_EXPORTER_OTLP_ENDPOINT")
-        .or_else(|_| std::env::var("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"))
-        .unwrap_or_else(|_| "http://127.0.0.1:4317".to_string());
-    let exporter = opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_endpoint(endpoint_grpc);
+    let protocol = std::env::var("OTEL_EXPORTER_OTLP_TRACES_PROTOCOL")
+        .or_else(|_| std::env::var("OTEL_EXPORTER_OTLP_PROTOCOL"))
+        .unwrap_or_else(|_| "http/protobuf".to_string());
+    let endpoint = std::env::var("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT")
+        .or_else(|_| std::env::var("OTEL_EXPORTER_OTLP_ENDPOINT"))
+        .ok();
+
+    let exporter: SpanExporterBuilder = match protocol.as_str() {
+        "http/protobuf" => opentelemetry_otlp::new_exporter()
+            .http()
+            .with_endpoint(endpoint.unwrap_or_else(|| "http://localhost:4318".to_string())) //Devskim: ignore DS137138
+            .into(),
+        _ => opentelemetry_otlp::new_exporter()
+            .tonic()
+            .with_endpoint(endpoint.unwrap_or_else(|| "http://localhost:4317".to_string())) //Devskim: ignore DS137138
+            .into(),
+    };
+
     let mut pipeline = opentelemetry_otlp::new_pipeline()
         .tracing()
         .with_exporter(exporter)
