e351d44b5a87b13a2864cd0d58159985471a173e
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2023-04-11 11:09:48 -0500

    Add example for use with gRPC

---
+++ examples/grpc/src/server.rs
@@ -0,0 +1,46 @@
+use axum_tracing_opentelemetry::opentelemetry_tracing_layer_grpc;
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request from {:?}", request.remote_addr());
+
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // very opinionated init of tracing, look as is source to make your own
+    axum_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
+        .expect("init subscribers");
+
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
+
+    println!("GreeterServer listening on {}", addr);
+
+    Server::builder()
+        .layer(opentelemetry_tracing_layer_grpc())
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
