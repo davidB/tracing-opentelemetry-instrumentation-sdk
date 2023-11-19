d8859542f80cf0df365ee18c3fcce654e2e1a843
David Bernard <david.bernard.31@gmail.com>
2023-11-19 20:15:15 +0100

    ✨ add attribute `rpc.grpc.status_code`

--- tracing-opentelemetry-instrumentation-sdk/src/http/grpc_server.rs
+++ tracing-opentelemetry-instrumentation-sdk/src/http/grpc_server.rs
@@ -1,47 +1,33 @@
 use crate::http::{extract_service_method, http_host, user_agent};
-use crate::BoxError;
-use crate::TRACING_TARGET;
+use crate::{BoxError, TRACING_TARGET};
 use tracing::field::Empty;
 
+use super::grpc_update_span_from_response;
+
 //TODO create similar but with tonic::Request<B> ?
+/// see [Semantic Conventions for gRPC | OpenTelemetry](https://opentelemetry.io/docs/specs/semconv/rpc/grpc/#grpc-status)
 pub fn make_span_from_request<B>(req: &http::Request<B>) -> tracing::Span {
     let (service, method) = extract_service_method(req.uri());
     tracing::trace_span!(
         target: TRACING_TARGET,
         "GRPC request",
         http.user_agent = %user_agent(req),
-        http.status_code = Empty, // to set on response
         otel.name = format!("{service}/{method}"),
         otel.kind = ?opentelemetry::trace::SpanKind::Server,
         otel.status_code = Empty,
         rpc.system ="grpc",
         rpc.service = %service,
         rpc.method = %method,
+        rpc.grpc.status_code = Empty, // to set on response
         server.address = %http_host(req),
         exception.message = Empty, // to set on response
         exception.details = Empty, // to set on response
     )
 }
 
-//TODO update behavior for grpc
-//TODO create similar but with tonic::Response<B> ?
-pub fn update_span_from_response<B>(span: &tracing::Span, response: &http::Response<B>) {
-    let status = response.status();
-    span.record(
-        "http.status_code",
-        &tracing::field::display(status.as_u16()),
-    );
-
-    if status.is_server_error() {
-        span.record("otel.status_code", "ERROR");
-    } else {
-        span.record("otel.status_code", "OK");
-    }
-}
-
-pub fn update_span_from_error(span: &tracing::Span, error: &BoxError) {
+fn update_span_from_error(span: &tracing::Span, error: &BoxError) {
     span.record("otel.status_code", "ERROR");
-    span.record("http.grpc_status", 1);
+    span.record("rpc.grpc.status_code", 2);
     span.record("exception.message", error.to_string());
     error
         .source()
@@ -54,7 +40,7 @@ pub fn update_span_from_response_or_error<B>(
 ) {
     match response {
         Ok(response) => {
-            update_span_from_response(span, response);
+            grpc_update_span_from_response(span, response, true);
         }
         Err(err) => {
             update_span_from_error(span, err);
