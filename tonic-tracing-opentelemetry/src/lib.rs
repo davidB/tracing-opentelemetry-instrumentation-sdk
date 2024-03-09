fc2f4049130d24729b356cb56dc84eed1ad80123
David Bernard <david.bernard.31@gmail.com>
2024-03-09 17:08:52 +0100

    🚨 disable linter for vendored code
    Signed-off-by: David Bernard <david.bernard.31@gmail.com>

--- tonic-tracing-opentelemetry/src/lib.rs
+++ tonic-tracing-opentelemetry/src/lib.rs
@@ -9,4 +9,5 @@ pub mod middleware;
 
 // HACK vendor of tracing_opentelemetry_instrumentation_sdk until tonic can support hyper 1, http 1, ...
 // TODO reexport tracing_opentelemetry_instrumentation_sdk crate
+#[allow(dead_code)]
 pub mod tracing_opentelemetry_instrumentation_sdk;
