c9eef50791837b2d256141f993332463673ceb4a
cozyGalvinism <jean@der-capta.in>
2026-04-22 16:46:42 +0200

    feat(examples): extend logging example with more levels

--- examples/logging/src/main.rs
+++ examples/logging/src/main.rs
@@ -1,4 +1,13 @@
 #[tracing::instrument]
+async fn log() {
+    tracing::error!("This is ground control to Major Tom");
+    tracing::warn!("Houston, we have a problem");
+    tracing::info!("We have contact");
+    tracing::debug!("Roger, copy that");
+    tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+}
+
+#[tracing::instrument]
 async fn calc(a: i32, b: i32) {
     let result = a + b;
     tracing::info!(result, "calculated result");
@@ -9,6 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // setting up tracing
     let _guard = init_tracing_opentelemetry::TracingConfig::production().init_subscriber()?;
 
+    log().await;
     calc(1, 2).await;
 
     Ok(())
